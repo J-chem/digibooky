fcebbc132d10a9c9ec98983e11e32be712cd18a4
Jan De Pelsmacker <jan.depelsmacker@gmail.com>
2021-11-29 13:45:30 +0100

    digibooky - fixed exception

--- src/main/java/com/switchfully/digibooky/api/BookController.java
+++ src/main/java/com/switchfully/digibooky/api/BookController.java
@@ -2,11 +2,9 @@ package com.switchfully.digibooky.api;
 
 import com.switchfully.digibooky.domain.Book;
 import com.switchfully.digibooky.services.BookService;
+import com.switchfully.digibooky.services.dtos.BookDTO;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/books")
@@ -23,4 +21,19 @@ public class BookController {
     public String postNewBook(Book createBookDto){
         return bookService.save(createBookDto);
     }
+
+    @GetMapping(produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public BookDTO getBookByTitle(@RequestParam String title){
+        if (title.equals("1")) {
+          BookDTO bookDto =  new BookDTO();
+          bookDto.setTitle("Umpa lumpa");
+          bookDto.setId("1");
+          bookDto.setAuthor(new Author("Jan", "D.P"));
+          return bookDto;
+        }
+        else throw  new ObjectNotFoundException("Could not find in Database");
+    }
+
+
 }

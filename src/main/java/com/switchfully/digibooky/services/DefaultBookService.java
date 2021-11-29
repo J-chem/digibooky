f74bfb1dc2e5eb25a30f6a984223a56b5e8aac13
Krike03 <deschutter.christophe@gmail.com>
2021-11-29 11:04:26 +0100

    repository save book

--- src/main/java/com/switchfully/digibooky/services/DefaultBookService.java
+++ src/main/java/com/switchfully/digibooky/services/DefaultBookService.java
@@ -1,5 +1,15 @@
 package com.switchfully.digibooky.services;
 
+import com.switchfully.digibooky.domain.Book;
+import com.switchfully.digibooky.repositories.BookRepository;
+import com.switchfully.digibooky.services.dtos.CreateBookDTO;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DefaultBookService implements BookService {
 
+    @Override
+    public String save(Book createBookDTO) {
+        return bookRepository.save(createBookDTO);
+    }
 }

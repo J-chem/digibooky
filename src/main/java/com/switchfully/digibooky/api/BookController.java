50c04d5c452d587cda83cdfa4b6d900edfa7026e
Krike03 <deschutter.christophe@gmail.com>
2021-11-29 11:16:36 +0100

    controller post book (Book in stead of createBookDto)

--- src/main/java/com/switchfully/digibooky/api/BookController.java
+++ src/main/java/com/switchfully/digibooky/api/BookController.java
@@ -1,7 +1,11 @@
 package com.switchfully.digibooky.api;
 
+import com.switchfully.digibooky.domain.Book;
 import com.switchfully.digibooky.services.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +16,11 @@ public class BookController {
 
     public BookController(BookService bookService) {
         this.bookService = bookService;
+    }
+
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String postNewBook(Book createBookDto){
+        return bookService.save(createBookDto);
     }
 }

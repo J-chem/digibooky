2850f4c950e9bcdb713f837f97e2e2d4f339f4da
Krike03 <deschutter.christophe@gmail.com>
2021-11-29 11:21:12 +0100

    controller add wildcard in import

--- src/main/java/com/switchfully/digibooky/api/BookController.java
+++ src/main/java/com/switchfully/digibooky/api/BookController.java
@@ -3,10 +3,8 @@ package com.switchfully.digibooky.api;
 import com.switchfully.digibooky.domain.Book;
 import com.switchfully.digibooky.services.BookService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping(path = "/books")

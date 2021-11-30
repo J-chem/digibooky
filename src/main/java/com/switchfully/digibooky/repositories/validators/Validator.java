679a5329f7422a9ed031ca15603504e538cc846e
Maxim Verbeeck <maximverbeeck@hotmail.com>
2021-11-30 14:45:08 +0100

    digi - added generic validator method

--- src/main/java/com/switchfully/digibooky/repositories/validators/Validator.java
+++ src/main/java/com/switchfully/digibooky/repositories/validators/Validator.java
@@ -1,12 +1,21 @@
 package com.switchfully.digibooky.repositories.validators;
 
+import com.switchfully.digibooky.custom.exceptions.EmptyBooksListException;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class Validator {
     public static void assertStringNotNull(String arg, String param) {
         if (arg == null){
             throw new IllegalArgumentException("The " + param + " can't be null");
+        }
+    }
+
+    public static <T, E> void assertBooksIsEmpty(ConcurrentHashMap<T, E> map) {
+        if (map.isEmpty()){
+            throw new EmptyBooksListException("List of books is empty");
         }
     }
 

493e3b63fdb07f10a2c94f03ad716a7b0f335ef7
Maxim Verbeeck <maximverbeeck@hotmail.com>
2021-11-30 09:59:48 +0100

    digi - made security exceptions and the handlers

--- src/main/java/com/switchfully/digibooky/api/ControllerExceptionHandler.java
+++ src/main/java/com/switchfully/digibooky/api/ControllerExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.switchfully.digibooky.api;
 
 import com.switchfully.digibooky.custom.exceptions.ObjectNotFoundException;
+import com.switchfully.digibooky.custom.exceptions.UnauthorizedException;
+import com.switchfully.digibooky.custom.exceptions.UnknownUserException;
+import com.switchfully.digibooky.custom.exceptions.WrongPasswordException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -19,5 +22,22 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
         response.sendError(NOT_FOUND.value(), onfe.getMessage());
     }
 
+    @ExceptionHandler(UnauthorizedException.class)
+    protected void userNotAuthenticated(UnauthorizedException ue,
+                                        HttpServletResponse response) throws IOException {
+        response.sendError(NOT_FOUND.value(), ue.getMessage());
+    }
+
+    @ExceptionHandler(UnknownUserException.class)
+    protected void unknownUser(UnknownUserException uue,
+                               HttpServletResponse response) throws IOException{
+        response.sendError(NOT_FOUND.value(), uue.getMessage());
+    }
+
+    @ExceptionHandler(WrongPasswordException.class)
+    protected void wrongPassword (WrongPasswordException wpe,
+                                  HttpServletResponse response) throws IOException{
+        response.sendError(NOT_FOUND.value(), wpe.getMessage());
+    }
 
 }
---
+++ src/main/java/com/switchfully/digibooky/custom/exceptions/UnauthorizedException.java
@@ -0,0 +1,11 @@
+package com.switchfully.digibooky.custom.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException() {
+        this("You are not authorized to access this info. ");
+    }
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}

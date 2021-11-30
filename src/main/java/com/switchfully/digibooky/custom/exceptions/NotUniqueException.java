f56251821fba4e8d2936ced71bc731f3c2580eab
Maxim Verbeeck <maximverbeeck@hotmail.com>
2021-11-30 15:33:58 +0100

    digi - unique ssn and email (todo)

---
+++ src/main/java/com/switchfully/digibooky/custom/exceptions/NotUniqueException.java
@@ -0,0 +1,7 @@
+package com.switchfully.digibooky.custom.exceptions;
+
+public class NotUniqueException extends RuntimeException{
+    public NotUniqueException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/switchfully/digibooky/repositories/DefaultUserRepository.java
+++ src/main/java/com/switchfully/digibooky/repositories/DefaultUserRepository.java
@@ -1,13 +1,20 @@
 package com.switchfully.digibooky.repositories;
 
+import com.switchfully.digibooky.custom.exceptions.NotUniqueException;
 import com.switchfully.digibooky.domain.user.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.Locale;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 
 @Repository
 public class DefaultUserRepository {
+    private final Function <User, String> getSsnUser = user -> user.getSocialSecurityNumber().trim().toLowerCase();
+    private final Function <User, String> getEmailUser = user -> user.getEmail().trim().toLowerCase();
+
     private final ConcurrentHashMap<String, User> usersById;
 
     public DefaultUserRepository() {
@@ -15,7 +22,27 @@ public class DefaultUserRepository {
     }
 
     public User save(User user) {
-        usersById.put(user.getId(), user);
-        return user;
+        //TODO voorwaarden ook in service
+        if(checkUserSocialSecurityNumber(user) && checkEmailUser(user)){
+            usersById.put(user.getId(), user);
+            return user;
+        }
+        throw new NotUniqueException("Social security number" + user.getSocialSecurityNumber() +
+                "or email is not unique");
+    }
+
+    //TODO overzetten naar services
+    private boolean checkEmailUser(User userToCheck) {
+        return usersById.values()
+                .stream()
+                .map(getEmailUser)
+                .noneMatch(email -> email.equals(userToCheck.getEmail()));
+    }
+
+    public boolean checkUserSocialSecurityNumber(User userToCheck){
+        return usersById.values()
+                .stream()
+                .map(getSsnUser)
+                .noneMatch(ssn -> userToCheck.getSocialSecurityNumber().equals(ssn));
     }
 }

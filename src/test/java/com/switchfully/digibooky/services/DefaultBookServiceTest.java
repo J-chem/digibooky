b2574408eceae78a4def3a99ddb53ff6c482fe1f
Najima Sahloumi <nsahloum@gmail.com>
2021-11-30 14:51:15 +0100

    fixed bookServiceTest

--- src/test/java/com/switchfully/digibooky/services/DefaultBookServiceTest.java
+++ src/test/java/com/switchfully/digibooky/services/DefaultBookServiceTest.java
@@ -3,7 +3,6 @@ package com.switchfully.digibooky.services;
 import com.switchfully.digibooky.domain.Author;
 import com.switchfully.digibooky.domain.Book;
 import com.switchfully.digibooky.domain.user.Address;
-import com.switchfully.digibooky.domain.user.Member;
 import com.switchfully.digibooky.domain.user.User;
 import org.junit.jupiter.api.Test;
 
@@ -12,21 +11,4 @@ class DefaultBookServiceTest {
     void lendBook() {
 
     }
-//  private Book book;
-// // @Mock
-//  private DefaultBookService defaultBookService;
-//
-//  @BeforeEach
-//  void setUp(){
-//      book = new Book("jkljf12", "Marie", new Author("Marie", "Julie"));
-//  }
-//
-//    @Test
-//    void givenBookCheckThatBookDTOHasSameFields(){
-//        BookDTO newBookDto = defaultBookService.convertBookinBookDto(book);
-//        assertThat(newBookDto.getAuthor()).isEqualTo(book.getAuthor());
-//        assertThat(newBookDto.getIsbn()).isEqualTo(book.getIsbn());
-//        assertThat(newBookDto.getTitle()).isEqualTo(book.getTitle());
-//        assertThat(newBookDto.getId()).isEqualTo(book.getId());
-//    }
 }

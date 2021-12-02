7d6f83c7ae63ae6752329d438f8e2b4b9055bdfe
Maxim Verbeeck <maximverbeeck@hotmail.com>
2021-12-02 10:10:26 +0100

    digi - adjusted lombok BookDto

--- src/main/java/com/switchfully/digibooky/services/dtos/BookDTO.java
+++ src/main/java/com/switchfully/digibooky/services/dtos/BookDTO.java
@@ -2,10 +2,12 @@ package com.switchfully.digibooky.services.dtos;
 
 import com.switchfully.digibooky.domain.Author;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Getter
+@Setter
 public class BookDTO {
     private String id;
     private String isbn;
@@ -13,34 +15,4 @@ public class BookDTO {
     private Author author;
     private boolean isLentOut;
     private LocalDate dueDate;
-
-    public BookDTO setId(String id) {
-        this.id = id;
-        return this;
-    }
-
-    public BookDTO setIsbn(String isbn) {
-        this.isbn = isbn;
-        return this;
-    }
-
-    public BookDTO setTitle(String title) {
-        this.title = title;
-        return this;
-    }
-
-    public BookDTO setAuthor(Author author) {
-        this.author = author;
-        return this;
-    }
-
-    public BookDTO setLentOut(boolean lentOut) {
-        isLentOut = lentOut;
-        return this;
-    }
-
-    public BookDTO setDueDate(LocalDate dueDate){
-        this.dueDate = dueDate;
-        return this;
-    }
 }
